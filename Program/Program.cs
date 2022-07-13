de989685492fbe325fdfb40bfcc9fba5c9ddd538
aslupik <aslupik@genetec.com>
2022-07-13 14:45:53 -0400

    Add git submodule and use it

--- Program/Program.cs
+++ Program/Program.cs
@@ -1,2 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using CommonLib;
+
+var stuff = CommonApi.MyApi(new Input("A", 2));
+Console.WriteLine(stuff);
